a1f2bb3a1af34985b951b1bb217003d97d69c3ef
MounishaThondaladinne-WAL <mounisha.t@westagilelabs.com>
2022-03-24 11:57:04 +0530

    Adding

--- models/categories.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-var CategoriesSchema = new mongoose.Schema({
-  name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, required: true, maxLength: 100 },
-});
-module.exports = mongoose.model("Category", CategoriesSchema);
--- routes/categories.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const categoryController = require("../controllers/categories");
-router.get("/", categoryController.getCategories);
-router.post("/", categoryController.createCategories);
-router.delete("/:_id", categoryController.deleteCategories);
-router.put("/:_id", categoryController.editCategories);
-module.exports = router;
